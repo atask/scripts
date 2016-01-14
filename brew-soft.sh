150479dc9d6032ed790837799a2e282b6b157480
ᴉuᴉɥɔsɐʇ uɐllɐ <allan.taschini@gmail.com>
2016-01-15 00:04:58 +0100

    Add brew basic software installs and config

---
+++ brew-soft.sh
@@ -0,0 +1,27 @@
+brew tap homebrew/dupes
+
+brew install rsync
+
+brew install git
+brew install libav
+brew install vim
+brew install python
+brew install android-platform-tools
+brew install node
+brew install jhead
+
+brew cask install keka
+brew cask install vlc
+brew cask install dupeguru
+brew cask install gimp
+brew cask install qlstephen
+brew cask install java
+brew cask install eclipse-jee
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install opera
+brew cask install sourcetree
+
+git config --global user.name "Allan Taschini"
+git config --global user.email "allan.taschini@gmail.com"
+git config --global core.autocrlf input
